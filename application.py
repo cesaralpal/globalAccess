5b537d4cce0572a2b5ec3e779a6e0cd8d64b08ad
Julio Alcibar <jalcibar@VILLA.local>
2019-09-05 12:53:51 -0500

    se cre api de get

--- application.py
+++ application.py
@@ -18,4 +18,4 @@ def hello():
     return "Hello, Global Access!" """
 
 if __name__ == '__application__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
+    app.run(debug=True, host='https://globalaccesssk.azurewebsites.net', port=5000)
