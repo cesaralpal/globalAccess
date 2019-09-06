c861319a4f1e5a33c9790c5468cf05a273a2719c
Julio Alcibar <jalcibar@VILLA.local>
2019-09-06 17:01:33 -0500

    se agreaga archivo

--- application.py
+++ application.py
@@ -8,10 +8,10 @@ class sigfox(Resource):
    def get(self):
       args = request.args
       return args
-
-
 api.add_resource(sigfox,'/sigfox')
-app.run()
+
+if __name__ == '__application__':
+    app.run(host='127.0.0.1', port=8080, debug=True)
 """ @app.route("/")
 def hello():
     return "Hello, Global Access!" """
